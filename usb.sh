6d3e5a0645a3b8b2f2bd3bffdffd8fdf9d403630
Ali Farhat <alimf@umich.edu>
2026-07-17 17:43:18 -0700

    Added phylogeny citations

---
+++ usb.sh
@@ -0,0 +1,63 @@
+mv 12-18/ KINGSTON/
+mv 12-19_Essentail\ Oil\ Standards/ KINGSTON/
+mv 12-21_Diffuser\ Expmts_Cin\ and\ Lem\ Oils/ KINGSTON/
+mv 2023-12-15_Breath\ Samples.sld* KINGSTON/
+mv 2025-12-19/ KINGSTON/
+mv 2025-12-19_LuMINS-SPARK/ KINGSTON/
+mv 2025-12-19_LuMINS-SPARK\ -\ Copy/ KINGSTON/
+mv ASMS\ 2023\ \ -\ Houston/ KINGSTON/
+mv BOOTEX.LOG* KINGSTON/
+mv BreathCollectionLiveView/ KINGSTON/
+mv CLSR\ investigation\ timing/ KINGSTON/
+mv CT\ Scheduling\ Working_SB-4\ -\ Macros.xlsm* KINGSTON/
+mv CT\ Scheduling\ Working_SB-4.xlsm* KINGSTON/
+mv Chromatograms/ KINGSTON/
+mv Custom\ VOC\ estimations_high\ res.xlsx* KINGSTON/
+mv Diffuser\ experiments.PNG* KINGSTON/
+mv Diffuser\ experiments_zoomed.PNG* KINGSTON/
+mv EI\ Tune\ Results.ods* KINGSTON/
+mv EI\ Tune\ Results_Orbi60.ods* KINGSTON/
+mv FOUND.000/ KINGSTON/
+mv Filament\ Background.ods* KINGSTON/
+mv For\ Ali/ KINGSTON/
+mv From\ Suresh/ KINGSTON/
+mv GroupMeeting_LabUpdate_2023-Dec-20.pptx* KINGSTON/
+mv HDF_Group/ KINGSTON/
+mv IABR2024_Poster_Borden.pptx* KINGSTON/
+mv LOST.DIR/ KINGSTON/
+mv Lab-Supplies-Ordering_2024-Sept.xlsx* KINGSTON/
+mv Manuscript-VOC-Stability-Kiland-2_RM_sept11.docx* KINGSTON/
+mv Nonanal\ Chromatogram-spectra.png* KINGSTON/
+mv Orbi\ 43/ KINGSTON/
+mv Orbi\ 43.zip* KINGSTON/
+mv Orbi\ 60/ KINGSTON/
+mv Orbi\ 60\ aux\ heating\ error/ KINGSTON/
+mv Orbi\ 60.zip* KINGSTON/
+mv Owlstone/ KINGSTON/
+mv PLCO\ Scores/ KINGSTON/
+mv RAID-Abstract-Neral-Citral-MenthylAcetate.PNG* KINGSTON/
+mv RAIDfiles/ KINGSTON/
+mv Spark\ Data\ Discussion_2024-Feb-27.pptx* KINGSTON/
+mv System\ Volume\ Information/ KINGSTON/
+mv TFRI\ Monthly\ Meeting\ 6-Nov-2025.pptx* KINGSTON/
+mv TFRI\ Monthly\ Meeting\ 6-Nov-2025_update-TME.pptx* KINGSTON/
+mv TFRI\ retreat\ PRES\ 19-Feb--2026_SBslides.pptx* KINGSTON/
+mv TFRI\ retreat\ PRES\ 19-Feb--2026_SBslides_2.pptx* KINGSTON/
+mv TFRI\ scripts/ KINGSTON/
+mv VAMS\ presentation\ material/ KINGSTON/
+mv eugenol\ zoom.PNG* KINGSTON/
+mv eugenol.PNG* KINGSTON/
+mv gc-orbitrap\ flavor\ and\ fragrances/ KINGSTON/
+mv mainlib2023/ KINGSTON/
+mv octane-d18.PNG* KINGSTON/
+mv screening/ KINGSTON/
+mv spark\ csvs/ KINGSTON/
+mv spark\ data/ KINGSTON/
+mv ~$6portFA_Consecutive_CombinedSuite2.xlsm* KINGSTON/
+mv ~$CT\ Scheduling\ Working_SB-4.xlsm* KINGSTON/
+mv ~$Copy\ of\ IDEAL_BreathDiscovery_Participant\ Data_MASTER_SB_2.xlsx* KINGSTON/
+mv ~$IABR2024_Poster_Borden.pptx* KINGSTON/
+mv ~$Lab\ G\ Pre\ lab\ Talk.pptx* KINGSTON/
+mv ~$Lab\ Slide_2023-11-08.pptx* KINGSTON/
+mv ~$QAQC\ INSET\ ACETONITRILE\ Calibration_18.xlsx* KINGSTON/
+mv ~$Thermo\ Checkpoint_july7.pptx* KINGSTON/
